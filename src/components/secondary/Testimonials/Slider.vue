401e6cbad67fca5cde14ce801e80dfa0f740c9b3
Gabriel Serejo <gabriel_serejo@live.com>
2024-02-19 17:01:55 -0300

    :mag_right: Fix container issues causing accessibility bad grade

--- src/components/secondary/Testimonials/Slider.vue
+++ src/components/secondary/Testimonials/Slider.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="px-3">
+    <div class="relative px-3">
+        <div class="absolute inset-y-0 left-full z-10 w-1/3 bg-gradient-to-l from-primary-50 from-30% to-transparent" />
+        <div class="absolute inset-y-0 right-full z-10 w-1/3 bg-gradient-to-r from-primary-50 from-30% to-transparent" />
         <Swiper
             :modules="[
                 Autoplay, 
